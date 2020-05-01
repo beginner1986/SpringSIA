390fc027aa2408cb18dcfee2d3aeb1aa21a1506a
Adam Emieljaniuk <beginner1986@gmail.com>
2020-05-01 20:29:41 +0200

    Orders and data validation implemented.

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -4,9 +4,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -41,6 +45,19 @@ public class DesignTacoController {
 		
 		model.addAttribute("design", new Taco());
 		return "design";
+	}
+	
+	@PostMapping
+	public String processDesign(@Valid Taco design, Errors errors) {
+		
+		if(errors.hasErrors()) {
+			return "design";
+		}
+		
+		// TODO
+		log.info("Przetwarzanie kompozycji taco: " + design);
+		
+		return "redirect:/orders/current";
 	}
 	
 	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
